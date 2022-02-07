6297ce1e84a0e09686a330ecea8ffd58501f53b1
saranyellanki <saran.yellanki12@gmail.com>
2022-02-07 07:56:24 +0530

    [Saran] Refactored: to add dashboard component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import MiniDrawer from './pages/dashboard/Dashboard';
 import ForgotPass from './pages/forgot-password/ForgotPass';
 import ResetPass from './pages/reset-password/ResetPass';
 import SignIn from './pages/signin/SignIn';
@@ -7,10 +8,11 @@ import SignUp from './pages/signup/SignUp';
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
       {/* <SignIn /> */}
       {/* <ForgotPass /> */}
       {/* <ResetPass /> */}
+      <MiniDrawer />
     </div>
   );
 }
