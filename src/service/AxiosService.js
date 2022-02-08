92301fdab0e1983d1fe850d4dc9a6fd5194d80d1
saranyellanki <saran.yellanki12@gmail.com>
2022-02-08 10:39:06 +0530

    [Saran] Added: Axios service for api connection

---
+++ src/service/AxiosService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+class AxiosService {
+
+  get(url,data="",headers=false){
+    return axios.get(url,data,headers)
+  }
+
+  post(url,data="",headers=false){
+    return axios.post(url,data,headers)
+  }
+
+  put(url,data="",headers){
+    return axios.put(url,data,headers)
+  }
+
+}
+
+export default AxiosService;
