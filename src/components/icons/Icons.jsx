14898a159006be0e793ed4e412d2a377a04c284d
saranyellanki <saran.yellanki12@gmail.com>
2022-02-09 21:02:18 +0530

    [Saran] Refactored: to add individual div for each icon

--- src/components/icons/Icons.jsx
+++ src/components/icons/Icons.jsx
@@ -4,25 +4,37 @@ import ArchiveOutlined from '@mui/icons-material/ArchiveOutlined';
 import {IconButton} from '@mui/material';
 
 const Icons = () => {
-  return <div> 
+  return <div style={{display: 'flex',width: '80%',justifyContent: 'space-around'}}>
+    <div> 
     <IconButton>
       <AddAlertOutlined fontSize='small' />
     </IconButton>
+    </div>
+    <div>
     <IconButton>
       <PersonAddAlt1Outlined fontSize='small' />
     </IconButton>
+    </div>
+    <div>
     <IconButton>
       <ColorLensOutlined fontSize='small' />
     </IconButton>
+    </div>
+    <div>
     <IconButton>
       <ImageOutlined fontSize='small' />
     </IconButton>
+    </div>
+    <div>
     <IconButton>
       <ArchiveOutlined fontSize='small' />
     </IconButton>
+    </div>
+    <div>
     <IconButton>
       <MoreVertOutlined fontSize='small' />
     </IconButton>
+    </div>
   </div>;
 }
 
