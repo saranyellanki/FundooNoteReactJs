618dbd225e2da5c91857563d395eb66c07b4b0a7
saranyellanki <saran.yellanki12@gmail.com>
2022-02-18 12:29:52 +0530

    [Saran] Refactored: to add api for crud using axios

--- src/service/AxiosService.js
+++ src/service/AxiosService.js
@@ -2,16 +2,20 @@ import axios from 'axios';
 
 class AxiosService {
 
-  get(url,data="",headers=false){
+  get(url,data="",headers){
     return axios.get(url,data,headers)
   }
 
-  post(url,data="",headers=false){
+  post(url,data="",headers){
     return axios.post(url,data,headers)
   }
 
   put(url,data="",headers){
     return axios.put(url,data,headers)
+  }
+
+  delete(url,data="",headers){
+    return axios.delete(url,data,headers)
   }
 
 }
