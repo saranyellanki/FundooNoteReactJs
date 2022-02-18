e0289313e060308d115b044a70dd9b0a1c3b9251
saranyellanki <saran.yellanki12@gmail.com>
2022-02-18 12:23:03 +0530

    [Saran] Refactored: to add internal routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
-import MiniDrawer from './pages/dashboard/Dashboard';
-import ForgotPass from './pages/forgot-password/ForgotPass';
-import ResetPass from './pages/reset-password/ResetPass';
-import SignIn from './pages/signin/SignIn';
 import SignUp from './pages/signup/SignUp';
+import SignIn from './pages/signin/SignIn';
+import ForgotPass from './pages/forgot-password/ForgotPass';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from 'react-router-dom';
+import MiniDrawer from './pages/dashboard/Dashboard';
+import ResetPass from './pages/reset-password/ResetPass';
+import Archive from './pages/archive/Archive';
+import Trash from './pages/trash/Trash';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/Signin' element={<SignIn />} />
+          <Route path='/' element={<SignIn />} />
           <Route path='/Signup' element={<SignUp />} />
-          <Route path='/Dashboard' element={<MiniDrawer />} />
-          <Route path='/ResetPassword' element={<ResetPass />} />
+          <Route path='/Dashboard/*' element={<MiniDrawer />}>
+            <Route path='archive' element={<Archive />} />
+            <Route path='trash' element={<Trash />} />
+          </Route>
+          <Route path='/ResetPassword/:id' element={<ResetPass />} />
           <Route path='/ForgotPassword' element={<ForgotPass />} />
         </Routes>
       </Router>
